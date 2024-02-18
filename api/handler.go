4b1950b891a1b0f7bb3bda7bcd9033f965bf131d
ang-rui-yan <angruiyan@gmail.com>
2024-02-19 02:32:31 +0800

    added boiler for handler

--- api/handler.go
+++ api/handler.go
@@ -1,9 +1,32 @@
 package api
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 type teacherHandler struct {
     Service TeacherService
 }
 
 func NewTeacherHandler(svc TeacherService) teacherHandler{
     return teacherHandler{Service: svc}
+}
+
+func (h *teacherHandler) RegisterHandler(c *gin.Context) {
+	panic("not implemented")
+}
+
+
+func (h *teacherHandler) GetCommonStudentsHandler(c *gin.Context) {
+	panic("not implemented")
+}
+
+
+func (h *teacherHandler) SuspendHandler(c *gin.Context) {
+	panic("not implemented")
+}
+
+
+func (h *teacherHandler) RetrieveForNotificationsHandler(c *gin.Context) {
+	panic("not implemented")
 }
