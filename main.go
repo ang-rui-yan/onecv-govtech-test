3190d70454da16bb8ecc80a64727cfb30fba7edb
ang-rui-yan <angruiyan@gmail.com>
2024-02-19 02:36:13 +0800

    added routes and handler to all

--- main.go
+++ main.go
@@ -1,16 +1,54 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"os"
+	"studentadmin/api"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	router := gin.New()
 
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Println("Error has occurred on .env file. Please check.")
+	}
+
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	user := os.Getenv("USER")
+	dbname := os.Getenv("DB_NAME")
+	pass := os.Getenv("PASSWORD")
+
+	dbPool, _, err := api.NewDBPool(api.DatabaseConfig{
+        Username : user,
+        Password : pass,
+        Hostname : host,
+        Port : port,
+        DBName : dbname,
+    })
+
+    if err != nil {
+        log.Fatalf("unexpected error while tried to connect to database: %v\n", err)
+    }
+
+    defer dbPool.Close()
+
+
+    DB := api.NewDatabase(dbPool)
+    service := api.NewTeacherService(DB)
+    handler := api.NewTeacherHandler(service)
+
 	api := router.Group("/api")
 	
-	api.POST("/register")
-	api.GET("/commonstudents")
-	api.POST("/suspend")
-	api.POST("/retrievefornotifications")
+	api.POST("/register", handler.RegisterHandler)
+	api.GET("/commonstudents", handler.GetCommonStudentsHandler)
+	api.POST("/suspend", handler.SuspendHandler)
+	api.POST("/retrievefornotifications", handler.RetrieveForNotificationsHandler)
 
 	router.Run(":8080")
 }
