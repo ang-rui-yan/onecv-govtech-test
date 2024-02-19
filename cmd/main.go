480f40e58f410b676cc0bb8eb7984682ec8e9b6f
ang-rui-yan <angruiyan@gmail.com>
2024-02-19 11:33:21 +0800

    added workflow

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"os"
 	"studentadmin/api"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
@@ -13,16 +16,33 @@ func main() {
 	dbname := os.Getenv("DB_NAME")
 	pass := os.Getenv("PASSWORD")
 
-	dbPool, _, err := api.NewDBPool(api.DatabaseConfig{
-		Username: user,
-		Password: pass,
-		Hostname: host,
-		Port:     port,
-		DBName:   dbname,
-	})
+	maxRetries := 5
+	retryDelay := 5 * time.Second
 
-	if err != nil {
-		log.Fatalf("unexpected error while tried to connect to database: %v\n", err)
+	var dbPool *pgxpool.Pool
+
+	// Attempt to connect to the database with retries.
+	for i := 0; i < maxRetries; i++ {
+		var err error
+		dbPool, _, err = api.NewDBPool(api.DatabaseConfig{
+			Username: user,
+			Password: pass,
+			Hostname: host,
+			Port:     port,
+			DBName:   dbname,
+		})
+
+		if err == nil {
+			break
+		}
+
+		log.Printf("Failed to connect to database: %v\n", err)
+		if i < maxRetries-1 {
+			log.Printf("Retrying in %v...\n", retryDelay)
+			time.Sleep(retryDelay)
+		} else {
+			log.Fatal("Could not connect to database after retries")
+		}
 	}
 
 	defer dbPool.Close()
